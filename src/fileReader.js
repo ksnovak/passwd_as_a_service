1d2d2f35f850627becb439bd2a1b479f8d32bfa3
Kevin Novak <knovak8@gmail.com>
2018-08-11 02:14:51 -0700

    Added test to see if file exists
    Makes sure the file exists before trying to access it

--- src/fileReader.js
+++ src/fileReader.js
@@ -1,13 +1,17 @@
 import fs from 'fs';
 import minimist from 'minimist'
+import 'babel-polyfill'
+import {promisify} from 'es6-promisify'
 
 
 module.exports = {
+	//Default options to use for command-line args
 	minimistOptions: {
 		string: ['passwd', 'groups'],
 		default: {passwd: '/etc/passwd', groups: '/etc/groups'}
 	},
 
+	//Interpret the command-line arguments passed, using defaults as needed
 	parseArgs: function(args) {
 		let parsedArgs = minimist(args, this.minimistOptions);
 
@@ -18,7 +22,19 @@ module.exports = {
 		return parsedArgs
 	},
 
-	// let readFile = function(filename) {
 
-	// }
+	//Check if the specified path is an existing file
+	doesFileExist: async function (path) {
+		try {
+			let stats = await fs(path)
+
+			//a successful search could be either a file or a directory. But here we only want files.
+			return stats.isFile()
+		}
+		catch (err) {
+			//If we got an error, then the file doesn't exist to us
+			return false
+		}
+	},
+
 }
--- src/server.js
+++ src/server.js
@@ -1,25 +1,14 @@
 import routes from './routes';
 import fileReader from './fileReader';
 import express from 'express';
-import minimist from 'minimist';
 
-const minimistArgs = {
-	string: ['passwd', 'groups'],
-	default: {passwd: '/etc/passwd', groups: '/etc/groups'}
-}
 const args = fileReader.parseArgs(process.argv)
 
-console.log(process.argv)
+const server = express();
 
-console.log(args)
-
-
-const app = express();
-
-routes(app)
-app.listen(3000, () => console.log('listening on port 3000'))
+routes(server)
+server.listen(3000, () => console.log('listening on port 3000'))
 
 module.exports = {
-	app,
-	minimistArgs
+	server
 }
