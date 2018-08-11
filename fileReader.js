915bbb15b3e5ad64ef6bc829e7f1f0bd9d2a8874
Kevin Novak <knovak8@gmail.com>
2018-08-10 18:39:53 -0700

    Adding module for file io

---
+++ fileReader.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+module.exports = function() {
+	let readFile = function(filename) {
+
+	}
+}
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import routes from './routes';
+import fileReader from './fileReader';
 import express from 'express';
 
 const app = express();
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello world!')
-// })
 routes(app)
 app.listen(3000, () => console.log('listening on port 3000'))
-
 
 module.exports = app
