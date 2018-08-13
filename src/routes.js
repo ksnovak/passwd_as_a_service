d7bb2de788ca206ec45a70bb2b25e4fbe06871f3
Kevin Novak <knovak8@gmail.com>
2018-08-13 16:29:27 -0700

    Initial routes specified

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,31 @@ module.exports = function(app) {
 	app.get('/entry', function(req, res) {
 		res.send('Hello world!')
 	})
+
+	app.get('/users', function(req, res) {
+
+	})
+
+	app.get('/users/query', function(req, res) {
+
+	})
+
+	app.get('/users/:uid', function(req, res) {
+
+	})
+
+	app.get('/users/:uid/groups', function(req, res) {
+
+	})
+
+
+	app.get('/groups', function(req, res) {
+
+	})
+	app.get('/groups/query', function(req, res) {
+
+	})
+	app.get('/groups/:gid', function(req, res) {
+
+	})
 }
